f2c7fe61631e040d46ab8b3d1b52207a8a09a4fc
John Swope <john@johnswope.com>
2017-09-14 08:21:32 -0700

    Finish merge

--- category.php
+++ category.php
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-<?php
-/**
- * The template for displaying Category pages
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @since Pluto 1.0
- */
-
-get_header(); ?>
-
-<div class="main-content-w">
-  <div class="main-content-m">
-    <?php os_the_primary_sidebar('left'); ?>
-    <div class="main-content-i">
-        <?php if ( have_posts() ) : ?>
-
-          <?php osetin_show_filter_bar('option'); ?>
-          <?php require_once(get_template_directory() . '/inc/set-layout-vars.php') ?>
-		  <?php require_once(get_template_directory() . '/inc/osetin-custom-index-query.php'); ?>
-          <div class="content side-padded-content">
-            <div class="index-isotope v3 <?php echo $isotope_class; ?>" data-layout-mode="<?php echo $layout_mode; ?>">
-              <?php $os_current_box_counter = 1; $os_ad_block_counter = 0; ?>
-              <?php
-              // Start the Loop.
-			     if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                   <?php get_template_part( 'v3-content', get_post_format() ); ?>
-                   <?php os_ad_between_posts(); ?>
-            	 <?php endwhile; endif; ?>
-	
-            </div>
-            <?php require_once(get_template_directory() . '/inc/isotope-navigation.php') ?>
-          </div>
-          <?php
-        else :
-
-          // If no content, include the "No posts found" template.
-          get_template_part( 'content', 'none' );
-        endif; ?>
-    </div>
-    <?php os_the_primary_sidebar('right'); ?>
-  </div>
-  <?php os_footer(); ?>
-</div>
-
-<?php
-get_footer();
-=======
 <?php
 /**
  * The template for displaying Category pages
@@ -96,4 +46,3 @@ get_header(); ?>
 
 <?php
 get_footer();
->>>>>>> 5ba5098b4e5d134e3700c84ea18ac8aae5999211
--- functions.php
+++ functions.php
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-<?php // Opening PHP tag - nothing should be before this, not even whitespace
-add_action( 'wp_enqueue_scripts', 'pluto_child_enqueue_styles' );
-function pluto_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
-
-
-
-}
-
-	  // Our custom post type function
-function create_posttype() {
-
-  register_post_type( 'tweet',
-  // CPT Options
-    array(
-      'labels' => array(
-        'name' => __( 'Tweets' ),
-        'singular_name' => __( 'Tweet' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'taxonomies'  => array( 'category' ),
-      'rewrite' => array('slug' => 'tweets'),
-    )
-  );
-}
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
-
-// Include tweeets inside archive.php
-function include_tweets( $query ) {
-  if( $query->is_date() || $query->is_category() && empty( $query->query_vars['suppress_filters'] ) ) {
-    $query->set( 'post_type', array(
-     'post', 'nav_menu_item', 'tweet'
-		));
-	  return $query;
-	}
-}
-add_filter( 'pre_get_posts', 'include_tweets' );
-
-add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
-function enqueue_load_fa() {
- 
-    wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
- 
-}
-=======
 <?php // Opening PHP tag - nothing should be before this, not even whitespace
 add_action( 'wp_enqueue_scripts', 'pluto_child_enqueue_styles' );
 function pluto_child_enqueue_styles() {
@@ -94,4 +42,3 @@ function include_tweets( $query ) {
 }
 add_filter( 'pre_get_posts', 'include_tweets' );
 
->>>>>>> 5ba5098b4e5d134e3700c84ea18ac8aae5999211
